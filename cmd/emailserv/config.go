fafe6aaa938c1a882fa1e971d2d27f29c818a79a
Mikołaj Baranowski <mikolajb@gmail.com>
2018-06-01 02:16:22 +0200

    client timeout can be provided through flags

--- cmd/emailserv/config.go
+++ cmd/emailserv/config.go
@@ -10,6 +10,7 @@ type configuration struct {
 	sendgrid struct {
 		key string
 	}
+	clientTimeout int
 }
 
 func (c *configuration) init() {
@@ -20,6 +21,7 @@ func (c *configuration) init() {
 	flag.StringVar(&c.amazon.key, "amazon.key", "", "amazon access key id")
 	flag.StringVar(&c.amazon.secret, "amazon.secret", "", "amazon secret access key")
 	flag.StringVar(&c.sendgrid.key, "sendgrid.key", "", "sendgrid key")
+	flag.IntVar(&c.clientTimeout, "client_timeout", 5000, "acceptable client work time in milliseconds")
 }
 
 func (c *configuration) parse() {
